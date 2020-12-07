c51ee37ab3aae7535f8029f0e66745a540b340cb
Kleber <klebernascimento@outlook.com.br>
2020-12-07 20:40:10 -0300

    Number: Alguns Cuidados

---
+++ Fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,7 @@
+console.log(7 / 0) // Infinity
+console.log("10" / 2) // 5 O JS faz a conversão do conteudo da string
+console.log("show!" * 2) // NaN
+console.log(0.1 + 0.7) // 0.7999999999999999 Por causa da espeficiação utilizada pelo JS esse calculo apresenta este resultado
+console.log(0.1 + 0.8) // 0.9
+//console.log(10.toString()) // SyntaxError: Invalid or unexpected token
+console.log((10.340).toFixed(2)) // 10.34
