dcd179b32a735676bc037be01c0f9c2742dce66c
Kleber <klebernascimento@outlook.com.br>
2020-12-08 12:52:30 -0300

    Exemplos Básicos de Funções #01

---
+++ Fundamentos/funcao1.js
@@ -0,0 +1,20 @@
+/* 
+    Função sem retorno
+*/
+function imprimirSoma(a, b){
+    console.log(a + b)
+}
+imprimirSoma(2,3) // 5
+imprimirSoma(2) // NaN
+imprimirSoma(2, 10, 1, 9, 10) // 12
+imprimirSoma() // NaN
+
+/**
+ * Função com retorno
+ */
+function soma(a, b = 0){
+    return a + b
+}
+console.log(soma(2,3)) // 5
+console.log(soma(2)) // 2
+console.log(soma()) // NaN
