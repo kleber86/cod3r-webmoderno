3507665c1d43ef45e28ac4742d13d6fbf8e2c3f0
Kleber <klebernascimento@outlook.com.br>
2020-12-09 20:12:45 -0300

    Usando a Estrutura FOR

---
+++ controle/for1.js
@@ -0,0 +1,17 @@
+// while
+let contador = 1
+while (contador <= 10) {
+    console.log(`Contador = ${contador}`)
+    contador++
+}
+
+// for #1
+for (let i = 1; i <= 10; i++) {
+    console.log(`Contador = ${i}`)
+}
+
+// for #2
+const notas = [6.7, 7.4, 9.8, 7.7]
+for (let i = 0; i < notas.length; i++) {
+    console.log(`nota = ${notas[i]}`)
+}
