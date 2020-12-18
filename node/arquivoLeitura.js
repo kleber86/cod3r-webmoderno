a5924c776415698b229ad0682eaccfce87a12b72
Kleber <klebernascimento@outlook.com.br>
2020-12-18 13:25:02 -0300

    Lendo Arquivos

---
+++ node/arquivoLeitura.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+
+const caminho = __dirname + '/arquivo.json'
+
+// Sicrono
+const conteudo = fs.readFileSync(caminho, 'utf-8')
+console.log(conteudo)
+
+// assincrono
+fs.readFile(caminho, 'utf-8', (err, conteudo) => {
+    const config = JSON.parse(conteudo)
+    console.log(`${config.db.host}:${config.db.port}`) // localhost:5432
+})
+
+const config = require('./arquivo.json')
+console.log(config.db) // { host: 'localhost', port: 5432, user: 'usuario', pass: '123456' }
+
+fs.readdir(__dirname, (err, arquivos) => {
+    console.log('Conteudo da pasta...')
+    console.log(arquivos)
+})
