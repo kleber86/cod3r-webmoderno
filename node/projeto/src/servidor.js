1716557bee909ea129ab60fe7b19407367f6c963
Kleber <klebernascimento@outlook.com.br>
2021-02-09 15:47:20 -0300

    Projeto: API com Express #03

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -16,9 +16,23 @@ app.get('/produtos/:id', (req, res, next) => {
 
 app.post('/produtos', (req, res, next) => {
     const produto = bancoDeDados.salvarProduto({
-    nome: req.body.name,
+    nome: req.body.nome,
     preco: req.body.preco
     })
+    res.send(produto)
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+    id: req.params.id,
+    nome: req.body.nome,
+    preco: req.body.preco
+    })
+    res.send(produto)
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
     res.send(produto)
 })
 
