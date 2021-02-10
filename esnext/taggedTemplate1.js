71ea93be9b61e51030df8db28f4cc8aadc1ef816
Kleber <klebernascimento@outlook.com.br>
2021-02-10 10:07:20 -0300

    Tagged Template #01

---
+++ esnext/taggedTemplate1.js
@@ -0,0 +1,15 @@
+// Tagged Templates - Processa o template dentro de uma função
+function tag(partes, ...valores){
+    console.log(partes)
+    console.log(valores)
+    return 'Outra string'
+}
+
+const aluno = 'Gui'
+const situação = 'Aprovado'
+console.log(tag `${aluno} está ${situação}.`)
+/**
+ [ '', ' está ', '.' ]
+ [ 'Gui', 'Aprovado' ]
+ Outra string
+*/
