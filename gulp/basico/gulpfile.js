26dd9ce250bbc02b8b27f300709eacb27b2307ae
Kleber <klebernascimento@outlook.com.br>
2021-02-26 12:43:41 -0300

    Fundamentos de Gulp #02

--- gulp/basico/gulpfile.js
+++ gulp/basico/gulpfile.js
@@ -1,9 +1,32 @@
 const gulp = require('gulp')
-const { series } = require('gulp')
+const { series, parallel } = require('gulp')
 
-function copiar(callback) {
-    console.log('Tarefa de copiar!')
+const antes1 = callback => {
+    console.log('Tarefa Antes 1')
     return callback()
 }
 
-module.exports.default = series(copiar)
+const antes2 = callback => {
+    console.log('Tarefa Antes 2')
+    return callback()
+}
+
+function copiar(callback) {
+    gulp
+        //.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt'])
+        .src('pastaA/**/*.txt')
+        //.pipe(acoesSobreOsArquivos)
+        .pipe(gulp.dest('pastaB'))
+    return callback()
+}
+
+const fim = callback => {
+    console.log('Tafera Fim!')
+    return callback()
+}
+
+module.exports.default = series(
+    parallel(antes1, antes2),
+    copiar,
+    fim
+)
