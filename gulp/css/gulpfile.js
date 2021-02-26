c43fdf484c3b3a4b5242465fb06e7196d2c5297b
Kleber <klebernascimento@outlook.com.br>
2021-02-26 13:59:13 -0300

    Processando SASS com Gulp #02

--- gulp/css/gulpfile.js
+++ gulp/css/gulpfile.js
@@ -6,11 +6,16 @@ const concat = require('gulp-concat')
 
 function transformacaoCSS() {
     return gulp.src('src/sass/index.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(uglifycss({ 'ugleComments': true }))
-        .pipe(concat('estilo.min.css'))
-        .pipe(gulp.dest('build/css'))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(uglifycss({ 'ugleComments': true }))
+    .pipe(concat('estilo.min.css'))
+    .pipe(gulp.dest('build/css'))
+    .pipe(copiarHTML())
 }
 
+function copiarHTML(){
+    return gulp.src('src/**/*.html')
+    .pipe(gulp.dest('build'))
+}
 
 exports.default = series(transformacaoCSS)
