cdc640e46ea9d731cdbb8cb2fad7c6cfda1c1183
Kleber <klebernascimento@outlook.com.br>
2021-02-28 11:13:36 -0300

    Configurando a Pasta de Saída

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,6 +1,13 @@
 const webpack = require('webpack')
 
 module.exports = {
-    mode: 'development',
-    entry: './src/principal.js'
+    mode: 'development', 
+    entry: './src/principal.js',
+    /**
+     * Configuração do nome e a saida do arquivo 
+     */
+    output: {
+        filename: 'principal.js',
+        path: __dirname + '/public'
+    }
 }
